ea99ba76559067922d1d0e5b92a2ffcc42687202
Daniel Morandini <danielmorandini@me.com>
2017-03-02 15:58:03 +0100

    :lipstick: Fix action bar issue

--- app/src/main/java/com/snowdays/snowdaysctrl/activities/MainActivity.java
+++ app/src/main/java/com/snowdays/snowdaysctrl/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.snowdays.snowdaysctrl.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -33,6 +35,17 @@ public class MainActivity extends BaseNetworkActivity<MainCard[]> {
         // specify an adapter
         mAdapter = new MainCardListAdapter(new ArrayList<MainCard>());
         mRecyclerView.setAdapter(mAdapter);
+
+        // Floating button actions
+        // TODO: Provide report action
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         // load data
         loadData();
